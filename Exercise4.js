8bfa24635d93907986442ddd253f0dbd80efd504
Panganiban Miguel <24022674@myrp.edu.sg>
2025-11-12 13:07:29 +0800

    updated Ex4 (made accurate to example)

--- Exercise4.js
+++ Exercise4.js
@@ -7,22 +7,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
+        backgroundColor: 'whitesmoke',
+        marginTop: 10
     },
     child: {
-
+        height: 80,
+        width: 80,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 });
 
 const Exercise4 = () => {
     return (
         <View style={styles.parent}>
-            <View style={[styles.child,{backgroundColor:'lightblue',padding:10}]}>
+            <View style={[styles.child,{backgroundColor:'skyblue'}]}>
                 <Text>Square 1</Text>
             </View>
-            <View style={[styles.child,{backgroundColor:'lightgreen',padding:10}]}>
+            <View style={[styles.child,{backgroundColor:'mediumaquamarine'}]}>
                 <Text>Square 2</Text>
             </View>
-            <View style={[styles.child,{backgroundColor:'red',padding:10}]}>
+            <View style={[styles.child,{backgroundColor:'crimson'}]}>
                 <Text>Square 3</Text>
             </View>
         </View>
